733faf0f71bfab509e69bcabb17dde12fe7ee9f0
soscolab <61241150+soscolab@users.noreply.github.com>
2021-05-10 08:26:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/loginapp/DetecteurDeClicSurRecycler.java
@@ -0,0 +1,9 @@
+package com.example.loginapp;
+
+import android.view.View;
+
+public interface DetecteurDeClicSurRecycler {
+
+    public void clicSurRecyclerItem(int position, View v);
+
+}
---
+++ app/src/main/java/com/example/loginapp/Donnee.java
@@ -0,0 +1,35 @@
+package com.example.loginapp;
+
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
+
+
+public class Donnee {
+        private String principal;
+        private String auxiliaire;
+         private Drawable image;
+
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    Donnee(String text1, String text2, Drawable image){
+            principal = text1;
+            auxiliaire = text2;
+            this.image= image;
+        };
+
+
+
+    public String getPrincipal() {
+            return principal;
+        }
+        public String getAuxiliaire() {
+            return auxiliaire;
+        }
+        public Drawable getimage() {
+        return image;
+    }
+
+
+
+}
+
